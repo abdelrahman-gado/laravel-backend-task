c67d90411d715d030ff3531f4858bc631676d5b4
abdelrahman-gado <abdelrahman.magdy.gado@gmail.com>
2023-07-09 02:19:23 +0300

    create Post_tag junction Model

---
+++ app/Models/Post_Tag.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Dyrynda\Database\Support\CascadeSoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post_Tag extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'post_tag';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'post_id',
+        'tag_id'
+    ];
+
+    /**
+     * Summary of post
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'post_id', 'id');
+    }
+
+    /**
+     * Summary of tag
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function tag()
+    {
+        return $this->belongsTo(Tag::class, 'tag_id', 'id');
+    }
+
+}
