2863b433bec5ab1fc360cb4a4679fc2d3bf2d79d
abdelrahman-gado <abdelrahman.magdy.gado@gmail.com>
2023-07-09 02:13:16 +0300

    create posts table

---
+++ database/migrations/2023_07_08_102627_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('body');
+            $table->string('coverImage');
+            $table->boolean('pinned')->default(false);
+            $table->unsignedBigInteger('user_id');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
