a3d69adabe484a14155ad4474bca6ab2ee9357f5
abdelrahman-gado <abdelrahman.magdy.gado@gmail.com>
2023-07-09 09:15:28 +0300

    create random user job to make a http request

---
+++ app/Jobs/RandomUser.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class RandomUser implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $response = Http::get('https://randomuser.me/api/');
+        info($response->json()); // we log the response to storage/logs/laravel.log
+    }
+}
